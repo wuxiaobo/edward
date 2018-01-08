9e753d428911fd2cefb8ccec5527e9efe47859aa
Dustin Tran <dustinviettran@gmail.com>
2018-01-08 01:04:01 -0800

    make {klpq,klqp,laplace,map,wake_sleep}.py,gans functional

--- examples/normal_normal.py
+++ examples/normal_normal.py
@@ -10,6 +10,7 @@ import numpy as np
 import tensorflow as tf
 
 from edward.models import Empirical, Normal
+from edward.util import get_session, Progbar
 
 ed.set_seed(42)
 
@@ -21,14 +22,22 @@ mu = Normal(loc=0.0, scale=1.0)
 x = Normal(loc=tf.ones(50) * mu, scale=1.0)
 
 # INFERENCE
-qmu = Empirical(params=tf.Variable(tf.zeros(1000)))
+qmu = Normal(loc=tf.Variable(0.0), scale=tf.nn.softplus(tf.Variable(1.0))+1e-3)
 
 # analytic solution: N(loc=0.0, scale=\sqrt{1/51}=0.140)
-inference = ed.HMC({mu: qmu}, data={x: x_data})
-inference.run()
+loss, grads_and_vars = ed.klqp({mu: qmu}, data={x: x_data})
 
-# CRITICISM
-sess = ed.get_session()
+train_op = tf.train.AdamOptimizer().apply_gradients(grads_and_vars)
+progbar = Progbar(1000)
+sess = get_session()
+tf.global_variables_initializer().run()
+for t in range(1, 1001):
+  loss_val, _ = sess.run([loss, train_op])
+  if t % 50 == 0:
+    progbar.update(t, {"Loss": loss_val})
+
+# # CRITICISM
+sess = get_session()
 mean, stddev = sess.run([qmu.mean(), qmu.stddev()])
 print("Inferred posterior mean:")
 print(mean)
@@ -36,12 +45,12 @@ print("Inferred posterior stddev:")
 print(stddev)
 
 # Check convergence with visual diagnostics.
-samples = sess.run(qmu.params)
+# samples = sess.run(qmu.params)
 
-# Plot histogram.
-plt.hist(samples, bins='auto')
-plt.show()
+# # Plot histogram.
+# plt.hist(samples, bins='auto')
+# plt.show()
 
-# Trace plot.
-plt.plot(samples)
-plt.show()
+# # Trace plot.
+# plt.plot(samples)
+# plt.show()
